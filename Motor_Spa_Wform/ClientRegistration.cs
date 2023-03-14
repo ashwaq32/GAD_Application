23d51037a9470f235bfefdd6eda190d03ab67f09
ashwaq32 <ashwaqsamman042@gmail.com>
2023-03-14 13:50:57 +0530

    added a client registration form
    added a client registration form and a button

---
+++ Motor_Spa_Wform/ClientRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Motor_Spa_Wform
+{
+    public partial class ClientRegistration : Form
+    {
+        public ClientRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
